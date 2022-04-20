03b1b79cf6ccc2da43de497be1e145047d712899
wotjd0963 <wotjd0963@naver.com>
2022-04-20 20:51:15 +0900

    BaseballGame class구현

--- include/BaseballGame.h
+++ include/BaseballGame.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class BaseballGame
 {
+public:
+    bool isGameDone();
+    void initialize();
+    int getNumOFstrike();
+    void input();
+    void judge();
+    void output();
 
+
+private:
+    void generate3DgitRandomNumber();
+
+private:
+    string answerNumberString;
+    string userNumberString;
+    int numOfStrike;
+    int numOfball;
+    bool isGameDone;
 };
